d9d7af5ea92778df6bf84f69296fc85d2b7141e3
Raphael-HdF <rdefrob@gmail.com>
2023-04-05 22:50:40 +0200

    [IMP] Refactoring to set oc_lettings_site as an app

--- site_config/settings.py
+++ site_config/settings.py
@@ -19,13 +19,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'oc_lettings_site.apps.OCLettingsSiteConfig',
+    'site_config.apps.SiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oc_lettings_site',
     'lettings',
     'profiles',
 ]
@@ -40,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'oc_lettings_site.urls'
+ROOT_URLCONF = 'site_config.urls'
 
 TEMPLATES = [
     {
@@ -58,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
+WSGI_APPLICATION = 'site_config.wsgi.application'
 
 
 # Database
